2c850a14acd3474ceade889146388883f6c78ba8
ReituTheConmpSciGirlie <nobuhlereitumettse@gmail.com>
2024-05-28 22:32:32 +0200

    Updated login and register

--- php/register.php
+++ php/register.php
@@ -7,7 +7,7 @@ session_start();
 $currentPage = 'register';
 
 if (isset($_SESSION['user_id'])) {
-    header('Location: https://cinetechwatch.000webhostapp.com/html/login.php'); // Redirect to login page if already logged in
+    header('Location: ../html/login.php'); // Redirect to login page if already logged in
     exit();
 }
 
@@ -43,7 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $ch = curl_init();
 
         // Set the URL
-        curl_setopt($ch, CURLOPT_URL, 'https://cinetechwatch.000webhostapp.com/php/api.php');
+        curl_setopt($ch, CURLOPT_URL, 'https://wheatley.cs.up.ac.za/u23535246/CINETECH/api.php');
 
         // Set the request method to POST
         curl_setopt($ch, CURLOPT_POST, 1);
@@ -56,7 +56,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         // Set basic authentication credentials
         curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($ch, CURLOPT_USERPWD, 'cinetechwatch:Cinetechwatch120%');
+        curl_setopt($ch, CURLOPT_USERPWD, 'u23535246:Toponepercent120');
         // Return response instead of outputting it
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
@@ -122,8 +122,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="referrer" content="strict-origin-when-cross-origin">
     <title>CineTech</title>
-    <link rel="stylesheet" href="https://cinetechwatch.000webhostapp.com/css/register-dark.css" id="dark-mode">
-    <link rel="icon" type="image/x-icon" href="https://cinetechwatch.000webhostapp.com/img/4.png">
+    <link rel="stylesheet" href="../CSS/register-dark.css" id="dark-mode">
+    <link rel="icon" type="image/x-icon" href="../img/4.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
